e77f1b118291e9004a3a63b88405fc38d226d9c6
Nirvana1997 <408985936@qq.com>
2022-03-24 00:19:31 +0800

    上传emptyTest

--- emptyTest/test.cpp
+++ emptyTest/test.cpp
@@ -7,6 +7,42 @@ using namespace std;
 
 #define X 1000000
 
+void checkEmpty(const set<int>& setNum)
+{
+  for (int i = 0; i < X; i++)
+  {
+    if (setNum.empty() == false)
+    {
+      for (int j : setNum)
+      {
+        cout << j << endl;
+      }
+    }
+  }
+}
+
+void noCheckEmpty(const set<int>& setNum)
+{
+  for (int i = 0; i < X; i++)
+  {
+    for (int j : setNum)
+    {
+      cout << j << endl;
+    }
+  }
+}
+
+void useIterator(const set<int>& setNum)
+{
+  for (int i = 0; i < X; i++)
+  {
+    for (auto it = setNum.begin(); it != setNum.end(); it++)
+    {
+      cout << *it << endl;
+    }
+  }
+}
+
 int main()
 {
   set<int> setNum;
@@ -16,40 +52,19 @@ int main()
   struct timeval start;
   struct timeval end;
   gettimeofday(&start, NULL);
-  for (int i = 0; i < X; i++)
-  {
-    if (setNum.empty() == false)
-    {
-      for (int j : setNum)
-      {
-        test++;
-      }
-    }
-  }
+  checkEmpty(setNum);
   gettimeofday(&end, NULL);
   time_use = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
   cout << "empty:" << time_use / 1000 << "ms" << endl;
 
   gettimeofday(&start, NULL);
-  for (int i = 0; i < X; i++)
-  {
-    for (int j : setNum)
-    {
-      test++;
-    }
-  }
+  noCheckEmpty(setNum);
   gettimeofday(&end, NULL);
   time_use = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
   cout << "no_empty,foreach:" << time_use / 1000 << "ms" << endl;
 
   gettimeofday(&start, NULL);
-  for (int i = 0; i < X; i++)
-  {
-    for (auto it = setNum.begin(); it != setNum.end(); it++)
-    {
-      test++;
-    }
-  }
+  useIterator(setNum);
   gettimeofday(&end, NULL);
   time_use = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
   cout << "no_empty,iterator:" << time_use / 1000 << "ms" << endl;
